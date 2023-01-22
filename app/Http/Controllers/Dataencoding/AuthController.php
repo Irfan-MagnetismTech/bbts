826f30eec2c29447a0e162abd7f6a563c042dca9
thrivuban51@yahoo.com <thrivuban51@yahoo.com>
2023-01-22 15:25:11 +0600

    user

---
+++ app/Http/Controllers/Dataencoding/AuthController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Dataencoding;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Register User and Token Creation
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function register(Request $request): JsonResponse
+    {
+        try {
+            $attr = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:users,email',
+                'password' => 'required|string|min:6|confirmed',
+//                'c_password' => 'required|same:password',
+            ]);
+
+            $user = User::create([
+                'name' => $attr['name'],
+                'password' => bcrypt($attr['password']),
+                'email' => $attr['email']
+            ]);
+
+            if ($user) {
+                $data = [
+                    'token' => $user->createToken($request->device_name)->plainTextToken,
+                    'user' => $user
+                ];
+                return response()->success('User Created Successfully', $data);
+            }
+        } catch (ValidationException $e) {
+            return response()->error($e->getMessage());
+        }
+    }
+
+    /**
+     * Login User and Token Creation
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(AuthRequest $request)
+    {
+        try {
+            $user = User::with('warehouse:id,name')->where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json(['success' => false, 'error' => 'Incorrect email/password !', 'message' => 'Incorrect email/password !'], 200);
+            }
+
+            $permissions = [];
+            foreach ($user->getRoleNames() as $role) {
+                $permissions[] = Role::whereName($role)->with('permissions')->get()->pluck('permissions')->flatten()->pluck('name')->toArray();
+            }
+            $allPermissions = [];
+            foreach ($permissions as $permission) {
+                foreach ($permission as $item) {
+                    if (!in_array($item, $allPermissions)) {
+                        $allPermissions[] = $item;
+                    }
+                }
+            }
+
+            $data = [
+                'token' => $user->createToken('Desktop')->plainTextToken,
+                'user' => [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'roles' => $user->getRoleNames(),
+                    'permissions' => $allPermissions,
+                    'warehouse' => $user->warehouse,
+                ]
+            ];
+
+            return response()->success('Login successful', $data, 200);
+        } catch (ValidationException $e) {
+            return response()->error($e->getMessage());
+        }
+    }
+
+    /**
+     * Logout User and Token Deletion
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return response()->success('Logout successful');
+        } catch (ValidationException $e) {
+            return response()->error('Logout failed');
+        }
+    }
+
+    public function export()
+    {
+        return Excel::download(new PurchaseOrdersExport, 'bpo.xlsx', \Maatwebsite\Excel\Excel::XLSX);
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+}
--- routes/dataencoding.php
+++ routes/dataencoding.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\Dataencoding\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Dataencoding\EmployeeController;
@@ -10,4 +12,6 @@ Route::prefix('dataencoding')->as('dataencoding.')->group(function ()
     Route::resource('departments', DepartmentController::class);
     Route::resource('designations', DesignationController::class);
     Route::resource('employees', EmployeeController::class);
+
+    Route::get('/', [AuthController::class, 'dashboard'])->name('dashboard');
 });
