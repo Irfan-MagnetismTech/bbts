a3bb4a1b3426da6eb31a9a56819d3006ee86acb1
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-01-25 17:35:03 +0600

    update: module stubs files location updated in config

--- Modules/Admin/Database/Migrations/2023_01_25_111326_create_materials_table.php
+++ Modules/Admin/Database/Migrations/2023_01_25_111326_create_materials_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('materials', function (Blueprint $table) {
             $table->id();
-
+            $table->string('name');
+            $table->string('unit');
+            $table->string('type');
+            $table->bigInteger('code');
             $table->timestamps();
         });
     }
