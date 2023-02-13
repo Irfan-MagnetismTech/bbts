91c559fe030f194487e9f60c3aaad6d05b04a1f9
Delowar Hossain <contact.delowar@gmail.com>
2023-02-13 15:45:52 +0600

    feat: working on Support Team Creation

--- Modules/Ticketing/Resources/views/teams/create-edit.blade.php
+++ Modules/Ticketing/Resources/views/teams/create-edit.blade.php
@@ -22,50 +22,54 @@
 @section('content')
     <div class="col-md-10 mx-auto">
         <div class="row">
-            <form
-            action="{{ ($formType == 'edit') ? route('support-teams.update', $requisition->id) : route('support-teams.store') }}"
-            method="post" class="custom-form">
-            @if ($formType == 'edit')
-                @method('PUT')
-            @endif
-            @csrf
-            
-                <div class="row">
-                    <div class="form-group col-6">
-                        <label for="mrs_no">Department Name:</label>
-                        <input type="text" class="form-control mrs_no" id="mrs_no" name="mrs_no" aria-describedby="mrs_no"
-                            value="{{ old('mrs_no') ?? ($team->mrs_no ?? '') }}" placeholder="Department Name">
+            <div class="col-md-12">
+                <form
+                action="{{ ($formType == 'edit') ? route('support-teams.update', $requisition->id) : route('support-teams.store') }}"
+                method="post" class="custom-form">
+                @if ($formType == 'edit')
+                    @method('PUT')
+                @endif
+                @csrf
+                
+                    <div class="row">
+                        <div class="form-group col-6">
+                            <label for="mrs_no">Department Name:</label>
+                            <input type="text" class="form-control mrs_no" id="mrs_no" name="mrs_no" aria-describedby="mrs_no"
+                                value="{{ old('mrs_no') ?? ($team->mrs_no ?? '') }}" placeholder="Department Name">
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="mrs_no">Department Head:</label>
+                            <input type="text" class="form-control mrs_no" id="mrs_no" name="mrs_no" aria-describedby="mrs_no"
+                                value="{{ old('mrs_no') ?? ($team->mrs_no ?? '') }}" placeholder="Department Name">
+                        </div>
                     </div>
-                    <div class="form-group col-6">
-                        <label for="mrs_no">Department Head:</label>
-                        <input type="text" class="form-control mrs_no" id="mrs_no" name="mrs_no" aria-describedby="mrs_no"
-                            value="{{ old('mrs_no') ?? ($team->mrs_no ?? '') }}" placeholder="Department Name">
+
+                    Team Members
+                    <div class="dt-responsive table-responsive">
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                            <tr>
+                                <th>#SL</th>
+                                <th>Name</th>
+                                <th>Designation</th>
+                                <th>Type</th>
+                                <th>Action</th>
+                                <th><i class="btn btn-primary btn-sm fa fa-plus add-new"></i></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
                     </div>
-                </div>
-
-                Team Members
-                <div class="dt-responsive table-responsive">
-                    <table class="table table-striped table-bordered">
-                        <thead>
-                        <tr>
-                            <th>#SL</th>
-                            <th>Name</th>
-                            <th>Designation</th>
-                            <th>Type</th>
-                            <th>Action</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
 
 @section('script')
-
+<script>
+    
+</script>
 @endsection
