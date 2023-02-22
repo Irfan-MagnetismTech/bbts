84b04b7b599484761fbce6eed4d6a128b52ee661
WebDevJahir <jahirulislam1994ctgbd@gmail.com>
2023-02-22 17:31:50 +0600

    Lead Generation create on progress

--- Modules/Sales/Resources/views/layouts/sidebar.blade.php
+++ Modules/Sales/Resources/views/layouts/sidebar.blade.php
@@ -1,18 +1,18 @@
 <div class="pcoded-navigation-label text-uppercase bg-primary">Sales</div>
 <ul class="pcoded-item pcoded-left-item">
     {{-- @hasanyrole('super-admin|admin') --}}
-    <li class="pcoded-hasmenu {{ request()->routeIs(['cs.*']) ? 'active pcoded-trigger' : null }}">
+    <li class="pcoded-hasmenu {{ request()->routeIs(['lead-generation.*']) ? 'active pcoded-trigger' : null }}">
         <a href="javascript:void(0)">
-            <span class="pcoded-micon"><i class="ti-panel"></i><b>D</b></span>
-            <span class="pcoded-mtext">Client</span>
+            <span class="pcoded-micon"><i class="fas fa-users"></i><b>D</b></span>
+            <span class="pcoded-mtext">Lead Generation</span>
             <span class="pcoded-mcaret"></span>
         </a>
         <ul class="pcoded-submenu">
-            <li class="{{ request()->routeIs('cs.create') ? 'active' : null }}">
-                <a href="{{ route('cs.create') }}"> <span class="pcoded-micon"><i class="ti-angle-right"></i></span><span class="pcoded-mtext">Create New</span><span class="pcoded-mcaret"></span></a>
+            <li class="{{ request()->routeIs('lead-generation.create') ? 'active' : null }}">
+                <a href="{{ route('lead-generation.create') }}"> <span class="pcoded-micon"><i class="ti-angle-right"></i></span><span class="pcoded-mtext">Create New</span><span class="pcoded-mcaret"></span></a>
             </li>
-            <li class="{{ request()->routeIs('cs.index') ? 'active' : null }}">
-                <a href="{{ route('cs.index') }}"> <span class="pcoded-micon"><i class="ti-angle-right"></i></span><span class="pcoded-mtext">List</span><span class="pcoded-mcaret"></span></a>
+            <li class="{{ request()->routeIs('lead-generation.index') ? 'active' : null }}">
+                <a href="{{ route('lead-generation.index') }}"> <span class="pcoded-micon"><i class="ti-angle-right"></i></span><span class="pcoded-mtext">List</span><span class="pcoded-mcaret"></span></a>
             </li>
         </ul>
     </li>
