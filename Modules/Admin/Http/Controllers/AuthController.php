4cb8dff93731f613c762ebc7342595b906cc609a
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-02-22 12:36:37 +0600

    fix: redirect on session expired

--- Modules/Admin/Http/Controllers/AuthController.php
+++ Modules/Admin/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
         $data = ['token' => $user->createToken('web')->plainTextToken];
 
-        return redirect()->route('dashboard');
+        return redirect()->intended();
     }
 
     public function logout()
