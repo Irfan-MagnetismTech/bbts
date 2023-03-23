0a8ba353d3ddf5b23f54dd00db41ac3beb9f78c0
Delowar Hossain <contact.delowar@gmail.com>
2023-03-23 15:34:51 +0600

    Sidebard design improved

--- Modules/Ticketing/Resources/views/layouts/sidebar.blade.php
+++ Modules/Ticketing/Resources/views/layouts/sidebar.blade.php
@@ -1,57 +1,57 @@
 {{-- Ticketing and Support --}}
 <div class="pcoded-navigation-label text-uppercase bg-primary">Support & Ticketing</div>
 <ul class="pcoded-item pcoded-left-item">
-    <li class="">
+    <li class="{{ request()->routeIs('support-tickets.create') ? 'active' : null }}">
         <a href="{{ route('support-tickets.create') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fa fa-ticket-alt"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fa fa-ticket-alt"></i></span>
             <span class="pcoded-mtext">New Ticket</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('support-tickets.index') ? 'active' : null }}">
         <a href="{{ route('support-tickets.index') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-clipboard-list"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-clipboard-list"></i></span>
             <span class="pcoded-mtext">Ticket List</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('forwarded-tickets') ? 'active' : null }}">
         <a href="{{ route('forwarded-tickets') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-fast-forward"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-fast-forward"></i></span>
             <span class="pcoded-mtext">Forwarded Ticket List</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('backwarded-tickets') ? 'active' : null }}">
         <a href="{{ route('backwarded-tickets') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="far fa-share-square"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="far fa-share-square"></i></span>
             <span class="pcoded-mtext">Backwarded Ticket List</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('bulk-email') ? 'active' : null }}">
         <a href="{{ route('bulk-email') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-mail-bulk"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-mail-bulk"></i></span>
             <span class="pcoded-mtext">Bulk Email</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('support-teams.index') ? 'active' : null }}">
         <a href="{{ route('support-teams.index') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-users"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-users"></i></span>
             <span class="pcoded-mtext">Support Teams</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('support-complain-types.index') ? 'active' : null }}">
         <a href="{{ route('support-complain-types.index') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-chalkboard-teacher"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-chalkboard-teacher"></i></span>
             <span class="pcoded-mtext">Complain Types</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('support-solutions.index') ? 'active' : null }}">
         <a href="{{ route('support-solutions.index') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-user-md"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-user-md"></i></span>
             <span class="pcoded-mtext">Support Solutions</span>
         </a>
     </li>
-    <li class="">
+    <li class="{{ request()->routeIs('complain-sources.index') ? 'active' : null }}">
         <a href="{{ route('complain-sources.index') }}">
-            <span class="pr-2 w-25px d-inline-block"><i class="fas fa-tty"></i></span>
+            <span class="pr-2 pt-1 w-25px d-inline-block"><i class="fas fa-tty"></i></span>
             <span class="pcoded-mtext">Complain Source</span>
         </a>
     </li>
