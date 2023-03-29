b598595ee3569c1e72c6b813e94ba4945350b325
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-03-29 15:22:11 +0600

    working on Gate pass

---
+++ Modules/SCM/Database/Migrations/2023_03_29_112351_create_scm_gate_passes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scm_gate_passes', function (Blueprint $table) {
+            $table->id();
+            $table->string('gate_pass_no');
+            $table->date('date')->nullable();
+            $table->string('carrier')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scm_gate_passes');
+    }
+};
--- Modules/SCM/Routes/jaber.php
+++ Modules/SCM/Routes/jaber.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\SCM\Http\Controllers\ScmMirController;
+use Modules\SCM\Http\Controllers\ScmGatePassController;
 use Modules\SCM\Http\Controllers\PurchaseOrderController;
 
 Route::get('search-material-price-by-cs-requisition/{csId}/{supplierId}/{materialId}', [PurchaseOrderController::class, 'searchMaterialPriceByCsAndRequsiition'])->name('search-material-price-by-cs-requisition');
@@ -10,7 +11,7 @@ Route::get('search-type-no', [ScmMirController::class, 'searchTypeNo'])->name('s
 Route::get('search-mrs--type-wise-aterials', [ScmMirController::class, 'mrsAndTypeWiseMaterials'])->name('mrsAndTypeWiseMaterials');
 Route::get('getMaterialStock', [ScmMirController::class, 'getMaterialStock'])->name('getMaterialStock');
 
-
 Route::resources([
     'material-issues' => ScmMirController::class,
+    'gate-passes' => ScmGatePassController::class,
 ]);
