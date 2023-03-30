b699fdc4f8e940c3957ddccc1f0ead7dad74eff5
Delowar Hossain <contact.delowar@gmail.com>
2023-03-30 10:51:56 +0600

    Websockets default dashboard url and website's favicon updated

--- config/websockets.php
+++ config/websockets.php
@@ -59,7 +59,7 @@ return [
     /*
      * This path will be used to register the necessary routes for the package.
      */
-    'path' => 'laravel-websockets',
+    'path' => 'D07zC3cAEe-3NyS9fPyQy-YcBG7UVJeU',
 
     /*
      * Dashboard Routes Middleware
