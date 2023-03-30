bc372ee1e493be939caf2546bdc151cc4d5c3391
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-03-30 15:20:14 +0600

    feat: wcr for supplier frontend on progress

---
+++ Modules/SCM/Entities/WarrentyFromSupplier.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\SCM\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WarrentyFromSupplier extends Model
+{
+    protected $guarded = [];
+}
---
+++ Modules/SCM/Entities/WarrentyFromSupplierLine.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\SCM\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WarrentyFromSupplierLine extends Model
+{
+    protected $guarded = [];
+}
