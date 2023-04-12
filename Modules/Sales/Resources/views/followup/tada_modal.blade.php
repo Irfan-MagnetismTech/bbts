485d51965cacfc06f33c1bc9e396d138da12236d
WebDevJahir <jahirulislam1994ctgbd@gmail.com>
2023-04-12 15:45:50 +0600

    follow up client question designed and migration generated

---
+++ Modules/Sales/Resources/views/followup/tada_modal.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="TaDaModal" tabindex="-1" aria-labelledby="TaDaModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">TA DA</h5>
+                <span type="button" class="btn-close" style="cursor: pointer; font-size:20px; color:red"
+                    data-dismiss="modal"><i class="fas fa-window-close"></i></span>
+            </div>
+            <div class="modal-body">
+                <input type="text" name="tada" class="form-control" id="tada" autocomplete="off"
+                    placeholder="TA DA" required value="">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-danger btn-outline-danger"
+                    data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-sm btn-primary btn-outline-primary ">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
