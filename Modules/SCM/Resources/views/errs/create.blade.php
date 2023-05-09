e87e3be3aadbca095eec49bee759bc151a3942ad
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-05-09 14:53:37 +0600

    fix: client details frontend

--- Modules/SCM/Resources/views/errs/create.blade.php
+++ Modules/SCM/Resources/views/errs/create.blade.php
@@ -116,23 +116,21 @@
     </div>
 
     <div class="row">
-
-
         <div class="form-group col-3 client_name">
             <label for="client_name">Client Name:</label>
             <input type="text" class="form-control" id="client_name" aria-describedby="client_name" name="client_name"
                 value="{{ old('client_name') ?? (@$client_name ?? '') }}" placeholder="Search...">
             <input type="hidden" name="client_id" id="client_id" value="{{ old('client_id') ?? @$client_id }}">
         </div>
-        <div class="form-group col-3 client_links">
-            <label for="select2">Client Links</label>
-            <select class="form-control select2" id="client_links" name="client_links">
-                <option value="" readonly selected>Select Client Link</option>
+        <div class="form-group col-3 fr_no">
+            <label for="select2">FR No</label>
+            <select class="form-control select2" id="fr_no" name="fr_no">
+                <option value="" readonly selected>Select FR No</option>
                 @if ($form_method == 'POST')
-                    <option value="{{ old('client_links') }}" selected>{{ old('client_links') }}</option>
+                    <option value="{{ old('fr_no') }}" selected>{{ old('fr_no') }}</option>
                 @endif
                 @if ($form_method == 'PUT')
-                    @foreach ($client_links as $clientInfo)
+                    @foreach ($fr_no as $clientInfo)
                         <option value="{{ $clientInfo->link_name }}"
                             data-fr-composite="{{ $clientInfo->fr_composite_key }}" data-fr-id="{{ $clientInfo->fr_id }}"
                             @selected($clientInfo->fr_composite_key == @$fr_composite_key)>
@@ -144,12 +142,11 @@
             <input type="hidden" name="fr_composite_key" id="fr_composite_key"
                 value="{{ old('fr_composite_key') ?? @$fr_composite_key }}">
         </div>
-
-        <div class="form-group col-3 fr_id">
-            <label for="fr_id">Fr ID:</label>
-            <input type="text" class="form-control" id="fr_id" aria-describedby="fr_id" name="fr_id" readonly
-                value="{{ old('fr_id') ?? (@$fr_id ?? '') }}">
-
+        <div class="form-group col-3 link_no">
+            <label for="link_no">Link No:</label>
+            <select class="form-control select2" id="link_no" name="link_no">
+                <option value="" readonly selected>Select Link No</option>
+            </select>
         </div>
         <div class="form-group col-3 client_no">
             <label for="client_no">Client No:</label>
@@ -157,11 +154,20 @@
                 value="{{ old('client_no') ?? (@$client_no ?? '') }}">
 
         </div>
+        <div class="form-group col-3 type">
+            <label for="type">Type:</label>
+            <select class="form-control select2" id="type" name="type">
+                <option value="service">Service Equipment</option>
+                <option value="link">Link</option>
+            </select>
+
+        </div>
 
         <div class="form-group col-3 client_address">
             <label for="client_address">Client Address:</label>
             <input type="text" class="form-control" id="client_address" name="client_address"
-                aria-describedby="client_address" readonly value="{{ old('client_address') ?? (@$client_address ?? '') }}">
+                aria-describedby="client_address" readonly
+                value="{{ old('client_address') ?? (@$client_address ?? '') }}">
         </div>
 
         <div class="form-group col-3 pop_name" style="display: none">
