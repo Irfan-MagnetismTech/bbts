f5a115cf5b53b7114d6eb2ab0819976daabc5777
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-05-09 10:57:14 +0600

    fix: client table

--- Modules/Sales/Database/Migrations/2023_02_02_064425_create_clients_table.php
+++ Modules/Sales/Database/Migrations/2023_02_02_064425_create_clients_table.php
@@ -15,9 +15,26 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('client_no')->nullable();
-            $table->string('name')->nullable();
-            $table->string('address')->nullable();
+            $table->integer('client_no')->nullable();
+            $table->string('client_name')->nullable();
+            $table->string('client_type')->nullable();
+            $table->string('reg_no')->nullable();
+            $table->date('date')->nullable();
+            $table->string('business_type')->nullable();
+            $table->integer('division_id')->nullable();
+            $table->integer('district_id')->nullable();
+            $table->integer('thana_id')->nullable();
+            $table->string('location')->nullable();
+            $table->string('lat_long')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->string('designation')->nullable();
+            $table->string('contact_no')->nullable();
+            $table->string('email')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('trade_license')->nullable();
+            $table->string('nid')->nullable();
+            $table->integer('branch_id')->nullable();
+            $table->integer('user_id')->nullable();
             $table->timestamps();
         });
     }
