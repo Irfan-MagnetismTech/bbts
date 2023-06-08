3f701aa8f8de8cf9fcf1dc5a4af44d4e28b443b1
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-06-08 17:53:05 +0600

    feat: working on pop crud

--- Modules/Admin/Resources/views/pops/create.blade.php
+++ Modules/Admin/Resources/views/pops/create.blade.php
@@ -34,8 +34,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-tagsinput.css') }}">
 @endsection
 @section('breadcrumb-button')
-    <a href="{{ route('pops.index') }}" class="btn btn-out-dashed btn-sm btn-warning"><i
-            class="fas fa-database"></i></a>
+    <a href="{{ route('pops.index') }}" class="btn btn-out-dashed btn-sm btn-warning"><i class="fas fa-database"></i></a>
 @endsection
 
 @section('sub-title')
@@ -52,52 +51,111 @@
         'encType' => 'multipart/form-data',
         'class' => 'custom-form',
     ]) !!}
-    <div class="row">
-        <div class="form-group col-3">
-            <label for="name">POP name:</label>
-            <input type="text" class="form-control" id="name" aria-describedby="name" name="name"
-                value="{{ $name }}" autocomplete="off" required>
-        </div>
-
-        {{-- <div class="form-group col-3">
-            <div class="form-item">
-                <input type="text" name="link_name" id="link_name" class="form-control" value="{{ $name }}" required>
-                <label for="link_name">Name of the link</label>
-            </div>
-        </div> --}}
-
-        <x-input-box colGrid="3" name="link_name" value="{{ $name }}" label="Name of the link" class="text-danger" />
+    <div class="row mb-2">
+        <x-input-box colGrid="3" name="name" value="{{ $name }}" label="POP name" attr="required" />
 
         <div class="form-group col-3">
-            <label for="pop_type">POP Type:</label>
             <select class="form-control" id="pop_type" name="pop_type" required>
                 <option value="" selected disabled>Select POP Type</option>
                 @foreach (config('businessinfo.popType') as $key => $value)
-                    <option value="{{ $key }}" {{ old('pop_type', !empty($pop) ? $pop->pop_type : null) == $key ? 'selected' : '' }}>
+                    <option value="{{ $key }}"
+                        {{ old('pop_type', !empty($pop) ? $pop->pop_type : null) == $key ? 'selected' : '' }}>
                         {{ $value }}</option>
                 @endforeach
             </select>
         </div>
 
+        <div class="form-group col-3">
+            <div class="input-group input-group-sm input-group-primary">
+                <select class="form-control" id="division_id" name="division_id" required>
+                    <option value="">Select division</option>
+                    @foreach (@$divisions as $division)
+                        <option value="{{ $division->id }}"
+                            {{ (old('division_id') ?? ($branch->division_id ?? '')) == $division->id ? 'selected' : '' }}>
+                            {{ $division->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
 
         <div class="form-group col-3">
-            <label for="applied_date">Applied Date:</label>
-            <input class="form-control applied_date" name="date" aria-describedby="applied_date"
-                value="{{ $applied_date }}" readonly placeholder="Select a Date" id="applied_date">
+            <div class="input-group input-group-sm input-group-primary">
+                <select class="form-control" id="district_id" name="district_id" required>
+                    <option value="">Select district</option>
+                    @if ($formType == 'edit')
+                        @foreach (@$districts as $district)
+                            <option value="{{ $district->id }}"
+                                {{ (old('district_id') ?? ($branch->district_id ?? '')) == $district->id ? 'selected' : '' }}>
+                                {{ $district->name }}
+                            </option>
+                        @endforeach
+                    @endif
+                </select>
+            </div>
         </div>
 
         <div class="form-group col-3">
-            <label for="date">PO Date:</label>
-            <input class="form-control po_date" name="po_date" aria-describedby="po_date" id="po_date"
-                value="{{ $po_date }}" readonly placeholder="PO Date">
+            <div class="input-group input-group-sm input-group-primary">
+                <select class="form-control" id="thana_id" name="thana_id" required>
+                    <option value="">Select thana</option>
+                    @if ($formType == 'edit')
+                        @foreach (@$thanas as $thana)
+                            <option value="{{ $thana->id }}"
+                                {{ (old('thana_id') ?? ($branch->thana_id ?? '')) == $thana->id ? 'selected' : '' }}>
+                                {{ $thana->name }}
+                            </option>
+                        @endforeach
+                    @endif
+                </select>
+            </div>
         </div>
 
-        <div class="form-group col-3 supplier_name">
-            <label for="supplier_name">Supplier Name:</label>
-            <input type="text" class="form-control supplier_name" aria-describedby="supplier_name" id="supplier_name"
-                name="supplier_name" value="{{ $supplier_name }}" placeholder="Supplier Name" readonly>
-            <input type="hidden" name="supplier_id" id="supplier_id" value="{{ $supplier_id }}">
+        <x-input-box colGrid="3" name="address" value="{{ $name }}" label="Address" />
+
+        <div class="form-group col-3">
+            <select name="branch_id" id="branch_id" class="form-control select2">
+                <option value="" selected disabled>Search Branch</option>
+            </select>
         </div>
+
+        <x-input-box colGrid="3" name="lat_long" value="{{ $name }}" label="Lat/Long" />
+        <x-input-box colGrid="3" name="owners_name" value="{{ $name }}" label="Owners Name" />
+        <x-input-box colGrid="3" name="contact_person" value="{{ $name }}" label="Contact Person" />
+        <x-input-box colGrid="3" name="designation" value="{{ $name }}" label="Designation" />
+        <x-input-box colGrid="3" name="contact_no" value="{{ $name }}" label="Contact No." />
+        <x-input-box colGrid="3" name="email" value="{{ $name }}" label="Email Address" />
+        <x-input-box colGrid="9" name="description" value="{{ $name }}" label="Description" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="Aproval Date" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="BTRC Aproval Date" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="Commissioning Date" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="Termination Date" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="Website Published Date" />
+        <x-input-box colGrid="3" name="applied_date" value="{{ $name }}" label="Next Renewal Date" />
+
+        <div class="col-md-3">
+            <div class="
+                     mt-2 mb-4">
+                <label class="mr-2" for="yes">Signboard</label>
+                <div class="form-check-inline">
+                    <label class="form-check-label" for="yes">
+                        <input type="radio" class="form-check-input radioButton" id="yes" name="signboard"
+                            value="yes" @checked(@$type == 'yes' || ($form_method == 'POST' && !old()))>
+                        Yes
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label" for="no">
+                        <input type="radio" class="form-check-input radioButton" id="no" name="signboard"
+                            value="no" @checked(@$type == 'no')>
+                        No
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <hr class="col-12">
+        
     </div>
 
     <div class="row">
@@ -111,88 +169,19 @@
 @endsection
 
 @section('script')
-    <script src="{{ asset('js/bootstrap-tagsinput.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.10.4/typeahead.bundle.min.js"></script>
-
+    <script src="{{ asset('js/custom-function.js') }}"></script>
     <script>
-        $(document).ready(function() {
-            $("#po_no").autocomplete({
-                source: function(request, response) {
-                    $.ajax({
-                        url: "{{ route('search_po_with_date') }}",
-                        type: 'get',
-                        dataType: "json",
-                        data: {
-                            search: request.term,
-                            po_type: 'Work Order'
-                        },
-                        success: function(data) {
-                            response(data);
-                        }
-                    });
-                },
-                select: function(event, ui) {
-                    $('#purchase_order_id').val(ui.item.value);
-                    $('#po_no').val(ui.item.label);
-                    $('#po_date').val(ui.item.date);
-                    $('#supplier_id').val(ui.item.supplier_id);
-                    $('#supplier_name').val(ui.item.supplier_name);
-                    return false;
-                }
-            })
+        // get data by associative dropdown
+        associativeDropdown("{{ route('get-districts') }}", 'division_id', '#division_id', '#district_id', 'get', null)
+        associativeDropdown("{{ route('get-thanas') }}", 'district_id', '#district_id', '#thana_id', 'get', null)
 
-            //using form custom function js file
-            fillSelect2Options("{{ route('searchBranch') }}", '#branch_id');
-            //using form custom function js file
+        select2Ajax("{{ route('searchBranch') }}", '#branch_id')
 
-            $('#applied_date').datepicker({
-                format: "dd-mm-yyyy",
-                autoclose: true,
-                todayHighlight: true,
-                showOtherMonths: true
-            });
-
-            @if (empty($pop) && empty(old('material_id')))
-                appendDataRow();
-            @endif
-            function appendDataRow() {
-                let row = `<tr>
-                            <td>
-                                <input type="text" name="material_name[]" class="form-control material_name" autocomplete="off">
-                                <input type="hidden" name="material_id[]" class="form-control material_id" autocomplete="off">
-                                <input type="hidden" name="item_code[]" class="form-control item_code" autocomplete="off"> 
-                                <input type="hidden" name="material_type[]" class="form-control material_type" autocomplete="off"> 
-                                <input type="hidden" name="receiveable_id[]" class="form-control receiveable_id" autocomplete="off"> 
-                            </td>
-                            <td class="form-group">
-                                <input type="text" name="brand_name[]" class="form-control brand_name" autocomplete="off" readonly>
-                                <input type="hidden" name="brand_id[]" class="form-control brand_id" autocomplete="off">
-                            </td>                            
-                            <td>
-                                <input type="text" name="model[]" class="form-control model" autocomplete="off" readonly>
-                            </td>
-                            <td>
-                                <input type="text" name="serial_code[]" class="form-control serial_code" autocomplete="off" readonly>
-                            </td>
-                            <td class="select2_container">
-                                <input type="text" name="unit[]" class="form-control unit" autocomplete="off" readonly>
-                            </td>
-                            <td>
-                                <i class="btn btn-danger btn-sm fa fa-minus remove-wor-row"></i>
-                            </td>
-                        </tr>
-                    `;
-                $('#wor_table tbody').append(row);
-            }
-            /* Adds and removes quantity row on click */
-            $("#wor_table")
-                .on('click', '.add-wor-row', () => {
-                    appendDataRow();
-                })
-                .on('click', '.remove-wor-row', function() {
-                    $(this).closest('tr').remove();
-                });
+        $('#applied_date').datepicker({
+            format: "dd-mm-yyyy",
+            autoclose: true,
+            todayHighlight: true,
+            showOtherMonths: true
         });
-
     </script>
 @endsection
