91e98b8183dca00fe83a21a8d4dd5c537f4310f7
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-06-11 10:51:43 +0600

    fix: attr added in input box component

--- resources/views/components/input-box.blade.php
+++ resources/views/components/input-box.blade.php
@@ -1,8 +1,9 @@
-@props(["colGrid", "name", "value", "label", "class" => "", "attr" => ""])
+@props(['colGrid', 'name', 'value', 'label', 'class' => '', 'attr' => ''])
 
 <div class="form-group col-{{ $colGrid }}">
     <div class="form-item">
-        <input type="text" name="{{ $name }}" id="{{ $name }}" class="form-control {{ $class }}" value="{{ $value }}" {{ $attr }}>
+        <input type="text" name="{{ $name }}" id="{{ $name }}" class="form-control {{ $class }}"
+            value="{{ $value }}" {{ $attr }}>
         <label for="{{ $name }}">{{ $label }}</label>
     </div>
 </div>
