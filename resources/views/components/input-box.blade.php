65be58cde1e8c73a88f15bc43729a601e7aea4b4
Irfan Majid <irfan@magnetismtech.com>
2023-06-11 12:21:32 +0600

    Fix:input form placeholder

--- resources/views/components/input-box.blade.php
+++ resources/views/components/input-box.blade.php
@@ -1,9 +1,9 @@
-@props(['colGrid', 'name', 'value', 'label', 'class' => '', 'attr' => ''])
+@props(['colGrid', 'name', 'value', 'label', 'class' => '', 'attr' => '','placeholder' =>''])
 
 <div class="form-group col-{{ $colGrid }}">
     <div class="form-item">
         <input type="text" name="{{ $name }}" id="{{ $name }}" class="form-control {{ $class }}"
-            value="{{ $value }}" {{ $attr }}>
+            value="{{ $value }}" {{ $attr }} placeholder="{{$placeholder}}">
         <label for="{{ $name }}">{{ $label }}</label>
     </div>
 </div>
