9ac4f461b1f3cdc9ca1502d69371634c5c69c430
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-07-09 15:24:39 +0600

    update: physical connectivity list page updated as new design

--- Modules/Networking/Routes/jaber.php
+++ Modules/Networking/Routes/jaber.php
@@ -2,10 +2,12 @@
 
 
 use Illuminate\Support\Facades\Route;
+use Modules\Networking\Http\Controllers\LogicalConnectivityController;
 use Modules\Networking\Http\Controllers\PhysicalConnectivityController;
 
 Route::resources([
     'physical-connectivities' => PhysicalConnectivityController::class,
+    'logical-connectivities' => LogicalConnectivityController::class,
 ]);
 Route::get('get-network-info-by-fr', [PhysicalConnectivityController::class, 'getNetworkInfoByFr'])->name('getNetworkInfoByFr');
 Route::get('get-challan-by-link-no', [PhysicalConnectivityController::class, 'getChallanInfoByLinkNo'])->name('getChallanInfoByLinkNo');
