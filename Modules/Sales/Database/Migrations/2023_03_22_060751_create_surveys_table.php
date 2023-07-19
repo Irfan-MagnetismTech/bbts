51ad2d59db67e0d795fbff0bdf86de9998824ebb
thrivuban51@yahoo.com <thrivuban51@yahoo.com>
2023-07-19 16:38:14 +0600

    migration changes

--- Modules/Sales/Database/Migrations/2023_03_22_060751_create_surveys_table.php
+++ Modules/Sales/Database/Migrations/2023_03_22_060751_create_surveys_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->integer('user_id')->nullable();
             $table->integer('branch_id')->nullable();
             $table->date('date')->nullable();
-            $table->string('connectivity_remarks')->nullable();
+            $table->string('survey_remarks')->nullable();
             $table->timestamps();
         });
     }
