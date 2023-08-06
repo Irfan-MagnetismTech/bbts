d1f50c4f762cd25ae2d0b77638f0f759211e19f3
irfan <irfan@magnetismtech.com>
2023-08-06 18:42:31 +0600

    Fiber cable done

--- Modules/Networking/Resources/views/net-fiber-management/create.blade.php
+++ Modules/Networking/Resources/views/net-fiber-management/create.blade.php
@@ -11,14 +11,22 @@
 
 @section('sub-title')
     <span class="text-danger">*</span> Marked are required.
+    @php
+        $pop_id = old('pop_id') ? old('pop_id') : (!empty($fiberManagement->pop_id) ? $fiberManagement->pop_id : null;
+        $connectivity_point_name = old('connectivity_point_name') ? old('connectivity_point_name') : (!empty($fiberManagement->connectivity_point_name) ? $fiberManagement->connectivity_point_name : null;
+        $cable_code = old('cable_code') ? old('cable_code') : (!empty($fiberManagement->cable_code) ? $fiberManagement->cable_code : null;
+        $parent_id = old('parent_id') ? old('parent_id') : (!empty($fiberManagement->parent_id) ? $fiberManagement->parent_id : null;
+        $fiberType = old('fiber_type') ? old('fiber_type') : (!empty($fiberManagement->fiber_type) ? $fiberManagement->fiber_type : null;
+        $coreNoColot = old('core_no_color') ? old('core_no_color') : (!empty($fiberManagement->core_no_color) ? $fiberManagement->core_no_color : null;
+    @endphp
 @endsection
 
 @section('content-grid', 'offset-md-1 col-md-10 offset-lg-2 col-lg-8 my-3')
 
 @section('content')
 
-    @if(isset($nestedmaterial))
-        {!! Form::open(array('url' => "networking/fiber-managements/$nestedmaterial->id",'method' => 'PUT','encType' =>"multipart/form-data", 'class'=>'custom-form')) !!}
+    @if(isset($fiberManagement))
+        {!! Form::open(array('url' => "networking/fiber-managements/$fiberManagement->id",'method' => 'PUT','encType' =>"multipart/form-data", 'class'=>'custom-form')) !!}
     @else
         {!! Form::open(array('url' => "networking/fiber-managements",'method' => 'POST','encType' =>"multipart/form-data", 'class'=>'custom-form')) !!}
     @endif
@@ -26,19 +34,19 @@
             <div class="col-md-12 col-xl-12">
                 <div class="input-group input-group-sm input-group-primary">
                     <label class="input-group-addon" for="pop_id">POP<span class="text-danger">*</span></label>
-                    {{Form::text('pop_id', old('pop_id') ? old('pop_id') : (!empty($nestedmaterial->pop_id) ? $nestedmaterial->pop_id : null),['class' => 'form-control','id' => 'pop_id', 'autocomplete'=>"off",'placeholder'=>"POP"])}}
+                    {{Form::text('pop_id', $pop_id),['class' => 'form-control','id' => 'pop_id', 'autocomplete'=>"off",'placeholder'=>"POP"])}}
                 </div>
             </div>
             <div class="col-md-12 col-xl-12">
                 <div class="input-group input-group-sm input-group-primary">
                     <label class="input-group-addon" for="connectivity_point_name">Connectivity Point Name<span class="text-danger">*</span></label>
-                    {{Form::text('connectivity_point_name', old('connectivity_point_name') ? old('connectivity_point_name') : (!empty($nestedmaterial->connectivity_point_name) ? $nestedmaterial->connectivity_point_name : null),['class' => 'form-control','id' => 'connectivity_point_name', 'autocomplete'=>"off",'required',])}}
+                    {{Form::text('connectivity_point_name', 5connectivity_point_name),['class' => 'form-control','id' => 'connectivity_point_name', 'autocomplete'=>"off",'required',])}}
                 </div>
             </div>
             <div class="col-md-12 col-xl-12">
                 <div class="input-group input-group-sm input-group-primary">
                     <label class="input-group-addon" for="cable_code">Cable Code<span class="text-danger">*</span></label>
-                    {{Form::text('cable_code', old('cable_code') ? old('cable_code') : (!empty($nestedmaterial->cable_code) ? $nestedmaterial->cable_code : null),['class' => 'form-control','id' => 'cable_code', 'autocomplete'=>"off",'required','placeholder'=>"Cable Code",])}}
+                    {{Form::text('cable_code', $cable_code),['class' => 'form-control','id' => 'cable_code', 'autocomplete'=>"off",'required','placeholder'=>"Cable Code",])}}
                 </div>
             </div>
 
@@ -47,14 +55,12 @@
                     <label class="input-group-addon" for="fiber_type">Type<span
                             class="text-danger">*</span></label>
                     <select class="form-control" id="fiber_type" name="fiber_type" required>
-                        <option value="">Select division</option>
+                        <option value="">Select Type</option>
                         @foreach (config('businessinfo.fiberType') as $key => $value)
-                            <option value="{{ $key }}">
+                            <option value="{{ $key }}" @if ($value == $fiberType) select @endif >
                                 {{ $value }}
                             </option>
                         @endforeach
-                        
-                        {{-- {{ (old('division_id') ?? ($branch->division_id ?? '')) == $division->id ? 'selected' : '' }} --}}
                     </select>
                 </div>
             </div>
@@ -64,8 +70,8 @@
                             class="text-danger">*</span></label>
                     <select class="form-control" id="core_no_color" name="core_no_color" required>
                         @foreach (config('businessinfo.coreNoColor') as $key => $value)
-                            <option value="{{ $key }}">
-                                {{ $value }}
+                            <option value="{{ $key }}" @if ($value == $coreNoColot) select @endif                        
+                             {{ $value }}
                             </option>
                         @endforeach
                     </select>
@@ -74,7 +80,7 @@
             <div class="col-md-12 col-xl-12">
                 <div class="input-group input-group-sm input-group-primary">
                     <label class="input-group-addon" for="parent_id">Core Ref ID.<span class="text-danger">*</span></label>
-                    {{Form::select('parent_id', $CoreRefIds, old('parent_id') ? old('parent_id') : (!empty($nestedmaterial->parent_id) ? $nestedmaterial->parent_id : null),['class' => 'form-control','id' => 'parent_id', 'placeholder'=>"Select Account Head Name", 'autocomplete'=>"off"])}}
+                    {{Form::select('parent_id', $CoreRefIds, $parent_id),['class' => 'form-control','id' => 'parent_id', 'placeholder'=>"Select Account Head Name", 'autocomplete'=>"off"])}}
                 </div>
             </div>
         </div><!-- end row -->
