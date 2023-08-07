6422a8e3a435f50c891ef83f212d56ecfec340ec
irfan-majid <irfan@magnetismtech.com>
2023-08-07 16:57:46 +0600

    Fix:payment date in sales

---
+++ Modules/Sales/Resources/views/sales/day-table.blade.php
@@ -0,0 +1,51 @@
+<div class="box bg-secondary">
+    <div style="">
+        <div class="">
+            <table class="table-condensed bg-secondary">
+                <tbody>
+                    <tr>
+                        <td class="day_td">1</td>
+                        <td class="day_td">2</td>
+                        <td class="day_td">3</td>
+                        <td class="day_td">4</td>
+                        <td class="day_td">5</td>
+                        <td class="day_td">6</td>
+                        <td class="day_td">7</td>
+                    </tr>
+                    <tr>
+                        <td class="day_td">8</td>
+                        <td class="day_td">9</td>
+                        <td class="day_td">10</td>
+                        <td class="day_td">11</td>
+                        <td class="day_td">12</td>
+                        <td class="day_td">13</td>
+                        <td class="day_td">14</td>
+                    </tr>
+                    <tr>
+                        <td class="day_td">15</td>
+                        <td class="day_td">16</td>
+                        <td class="day_td">17</td>
+                        <td class="day_td">18</td>
+                        <td class="day_td">19</td>
+                        <td class="day_td">20</td>
+                        <td class="day_td">21</td>
+                    </tr>
+                    <tr>
+                        <td class="day_td">22</td>
+                        <td class="day_td">23</td>
+                        <td class="day_td">24</td>
+                        <td class="day_td">25</td>
+                        <td class="day_td">26</td>
+                        <td class="day_td">27</td>
+                        <td class="day_td">28</td>
+                    </tr>
+                    <tr>
+                        <td class="day_td">29</td>
+                        <td class="day_td">30</td>
+                        <td class="day_td">31</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
