c4278ed1f2fa8c88d99b0779e11ec90a36ab55c5
irfan-majid <irfan@magnetismtech.com>
2023-08-08 16:52:43 +0600

    Fix::Fine Tuning

--- Modules/Admin/Resources/views/connectivities/create.blade.php
+++ Modules/Admin/Resources/views/connectivities/create.blade.php
@@ -46,9 +46,7 @@ legend {
     $form_heading = !empty($connectivity->id) ? 'Update' : 'Add';
     $form_url = !empty($connectivity->id) ? route('connectivity.update', $connectivity->id) : route('connectivity.store');
     $form_method = !empty($connectivity->id) ? 'PUT' : 'POST';
-
     $reference = old('reference', !empty($connectivity) ? $connectivity->reference : null);
-
     $increament_type = old('increament_type', !empty($connectivity) ? $connectivity->increament_type : null);
     $link_type = old('link_type', !empty($connectivity) ? $connectivity->link_type : null);
     $link_name = old('link_name', !empty($connectivity) ? $connectivity->link_name : null);
@@ -104,8 +102,6 @@ legend {
         'method' => $form_method,
         'class' => 'custom-form',
     ]) !!}
-
-    {{-- Comparative Statement --}}
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -136,7 +132,6 @@ legend {
                         <input type="hidden" id="vendor_id" name="vendor_id" value="{{$vendor_id}}">
                         <x-input-box colGrid="2" name="bbts_link_id" label="BBTS Link Id" value="{{$bbts_link_id}}"/>
                     </div>
-
                     <div class="row"> 
                         <div class="col-md-4">
                             <div class="col-md-12">
@@ -144,11 +139,9 @@ legend {
                                     <div class="col-12 col-md-12" style="margin-top: -11px;">
                                         <span class="section-label">  From Info</span>
                                     </div>
-                                    
                                     <x-input-box colGrid="12" name="from_location" label="From Location" value="{{$from_location}}" placeholder="POP"/>
                                     <input type="hidden" id="from_pop_id" name="from_pop_id" value="{{$from_pop_id}}">
                                     <x-input-box colGrid="12" name="from_site" label="From Site" value="{{$from_site}}" placeholder="LDP"/> 
-
                                 </div>
                             </div>
                         </div>
@@ -158,7 +151,6 @@ legend {
                                     <div class="col-12 col-md-12" style="margin-top: -11px;">
                                         <span class="section-label">  To Info </span>
                                     </div>
-                                    
                                     <div class="form-group col-4">
                                         <div class="input-group input-group-sm input-group-primary">
                                             <select class="form-control" id="division_id" name="division_id" required>
@@ -172,7 +164,6 @@ legend {
                                             </select>
                                         </div>
                                     </div>
-
                                     <div class="form-group col-4">
                                         <div class="input-group input-group-sm input-group-primary">
                                             <select class="form-control" id="district_id" name="district_id" required>
@@ -216,7 +207,6 @@ legend {
                     </div>    
                 </div>
             </div>
-            {{-- <hr class="text-danger"/> --}}
             <div class="card">
                 <div class="text-center mt-4 bg-info pt-2 pb-2">
                     <h5 class="text-dark"> <span> &#10070; </span> Technical Information <span>&#10070;</span> </h5>
@@ -226,16 +216,15 @@ legend {
                         <x-input-box colGrid="2" name="vendor_link_id" label="Vendor Link Id" value="{{$vendor_link_id}}"/>
                         <x-input-box colGrid="2" name="vendor_vlan" label="Vendor VLAN" value="{{$vendor_vlan}}"/>
                         <x-input-box colGrid="2" name="port" label="Port" value="{{$port}}"/>
-                        <x-input-box colGrid="3" name="date_of_commissioning" label="Date Of Commissioning" value="{{$date_of_commissioning}}" class="date"/>
-                        <x-input-box colGrid="3" name="date_of_termination" label="Date Of Termination" value="{{$date_of_termination}}" class="date"/>
+                        <x-input-box colGrid="3" name="date_of_commissioning" label="Date Of Commissioning" value="{{$date_of_commissioning}}" class="date" attr="readonly"/>
+                        <x-input-box colGrid="3" name="date_of_termination" label="Date Of Termination" value="{{$date_of_termination}}" class="date" attr="readonly"/>
                     </div>
                     <div class="row">
-                        <x-input-box colGrid="2" name="activation_date" label="Activation Date" value="{{$activation_date}}" class="date"/>
+                        <x-input-box colGrid="2" name="activation_date" label="Activation Date" value="{{$activation_date}}" class="date" attr="readonly"/>
                         <x-input-box colGrid="10" name="remarks" label="Remarks" value="{{$remarks}}"/>
                     </div>
                 </div>
             </div>
-            {{-- <hr class="text-danger"/> --}}
             <div class="card">
                 <div class="text-center mt-4 bg-info pt-2 pb-2">
                     <h5 class="text-dark"> <span> &#10070; </span> Billing Information <span>&#10070;</span> </h5>
@@ -299,50 +288,47 @@ legend {
                 })
                 $('#link_name').on('keyup', function() {
                     if($('input[name="link_type"]:checked').val() === 'existing'){
-                        let myObject = { 
-                                }
+                        let myObject = {}
                         jquaryUiAjax(this, "{{ route('get_location_info_for_link') }}", uiList, myObject);
-                       
                         function uiList(item) {
-                            $('#division_id').val(item.division_id).trigger('change');
-                            $('#link_name').val(item.link_name).attr('value',item.link_name);
-                            $('#vendor_id').val(item.vendor_id).attr('value',item.vendor_id);
-                            $('#vendor').val(item.vendor_name).attr('value',item.vendor_name);
-                            $('#bbts_link_id').val(item.bbts_link_id).attr('value',item.bbts_link_id);
-                            $('#from_location').val(item.from_location).attr('value',item.from_location);
-                            $('#from_site').val(item.from_site).attr('value',item.from_site);
-                            $('#vendor_link_id').val(item.vendor_link_id).attr('value',item.vendor_link_id);
-                            $('#vendor_vlan').val(item.vendor_vlan).attr('value',item.vendor_vlan);
-                            $('#port').val(item.port).attr('value',item.port);
-                            $('#date_of_commissioning').val(item.date_of_commissioning).attr('value',item.date_of_commissioning);
-                            $('#date_of_termination').val(item.date_of_termination).attr('value',item.date_of_termination);
-                            $('#activation_date').val(item.activation_date).attr('value',item.activation_date);
-                            $('#remarks').val(item.remarks).attr('value',item.remarks);
-                            $('#capacity_type').val(item.capacity_type).attr('value',item.capacity_type);
-                            $('#existing_capacity').val(item.existing_capacity).attr('value',item.existing_capacity);
-                            $('#new_capacity').val(item.new_capacity).attr('value',item.new_capacity);
-                            $('#terrif_per_month').val(item.terrif_per_month).attr('value',item.terrif_per_month);
-                            $('#amount').val(item.amount).attr('value',item.amount);
-                            $('#vat_percent').val(item.vat_percent).attr('value',item.vat_percent);
-                            $('#vat').val(item.vat).attr('value',item.vat);
-                            $('#total').val(item.total).attr('value',item.total);
-                            if(item.increament_type == 'increase'){
+                            $('#division_id').val(item.data.division_id).trigger('change');
+                            $('#link_name').val(item.data.link_name).attr('value',item.data.link_name);
+                            $('#vendor_id').val(item.data.vendor_id).attr('value',item.data.vendor_id);
+                            $('#vendor').val(item.data.vendor_name).attr('value',item.data.vendor_name);
+                            $('#bbts_link_id').val(item.data.bbts_link_id).attr('value',item.data.bbts_link_id);
+                            $('#from_location').val(item.data.from_location).attr('value',item.data.from_location);
+                            $('#from_site').val(item.data.from_site).attr('value',item.data.from_site);
+                            $('#vendor_link_id').val(item.data.vendor_link_id).attr('value',item.data.vendor_link_id);
+                            $('#vendor_vlan').val(item.data.vendor_vlan).attr('value',item.data.vendor_vlan);
+                            $('#port').val(item.data.port).attr('value',item.data.port);
+                            $('#date_of_commissioning').val(item.data.date_of_commissioning).attr('value',item.data.date_of_commissioning);
+                            $('#date_of_termination').val(item.data.date_of_termination).attr('value',item.data.date_of_termination);
+                            $('#activation_date').val(item.data.activation_date).attr('value',item.data.activation_date);
+                            $('#remarks').val(item.data.remarks).attr('value',item.data.remarks);
+                            $('#capacity_type').val(item.data.capacity_type).attr('value',item.data.capacity_type);
+                            $('#existing_capacity').val(item.data.existing_capacity).attr('value',item.data.existing_capacity);
+                            $('#new_capacity').val(item.data.new_capacity).attr('value',item.data.new_capacity);
+                            $('#terrif_per_month').val(item.data.terrif_per_month).attr('value',item.data.terrif_per_month);
+                            $('#amount').val(item.data.amount).attr('value',item.data.amount);
+                            $('#vat_percent').val(item.data.vat_percent).attr('value',item.data.vat_percent);
+                            $('#vat').val(item.data.vat).attr('value',item.data.vat);
+                            $('#total').val(item.data.total).attr('value',item.data.total);
+                            if(item.data.increament_type == 'increase'){
                                 $('#increase').attr('checked',true);
                             }else{
                                 $('#decrease').attr('checked',true);
                             }
-                            
-                            $('#teck_type').val(item.teck_type).attr('value',item.teck_type);
-                            if(item.teck_type == 'link'){
+                            $('#teck_type').val(item.data.teck_type).attr('value',item.data.teck_type);
+                            if(item.data.teck_type == 'link'){
                                 $('#link').attr('checked',true);
                             }else{
                                 $('#backbone').attr('checked',true);
                             }
                             $('#district_id').on('DOMNodeInserted', function() {
-                                $('#district_id').val(item.district_id).trigger('change');
+                                $('#district_id').val(item.data.district_id).trigger('change');
                             });
                             $('#thana_id').on('DOMNodeInserted',function() {
-                                $('#thana_id').val(item.thana_id).trigger('change');
+                                $('#thana_id').val(item.data.thana_id).trigger('change');
                             });
                             return false;
                         }
@@ -356,10 +342,8 @@ legend {
 
                 $('#from_location,#to_location').on('keyup', function(event) {
                     let selector = this;
-                    let myObject = { 
-                                }
+                    let myObject = {}
                         jquaryUiAjax(this, "{{ route('get_pop') }}", uiList, myObject);
-                       
                         function uiList(item) {
                             $(selector).val(item.label).attr('value',item.label);
                             if(event.target.id == "from_location"){
@@ -372,10 +356,8 @@ legend {
                 })
                 $('#from_site').on('keyup', function(event) {
                     let selector = this;
-                    let myObject = { 
-                                }
+                    let myObject = {}
                         jquaryUiAjax(this, "{{ route('get_link_sites') }}", uiList, myObject);
-                       
                         function uiList(item) {
                             $(selector).val(item.label).attr('value',item.label);
                             return false;
@@ -385,10 +367,8 @@ legend {
             associativeDropdown("{{ route('get-districts') }}", 'division_id', '#division_id', '#district_id', 'get', null)
             associativeDropdown("{{ route('get-thanas') }}", 'district_id', '#district_id', '#thana_id', 'get', null)
             $('#vendor').on('keyup', function() {
-                    let myObject = { 
-                    }
+                    let myObject = {}
                     jquaryUiAjax(this, "{{ route('get_vendors') }}", uiList, myObject);
-
                     function uiList(item) {
                         $('#vendor').val(item.label);
                         $('#vendor_id').val(item.id);
@@ -401,15 +381,13 @@ legend {
                
             })
             $(document).ready(function(){
-                @if (!empty($connectivity->id))
                     changeInput();
-                @endif
             })
             function changeInput(){
-                if($('input[name="link_type"]:checked').val() == 'new'){
-                    $('#bbts_link_id').parent().addClass('d-none');
-                }else{
+                if($('input[name="link_type"]:checked').val() == 'existing'){
                     $('#bbts_link_id').parent().removeClass('d-none');
+                }else{
+                    $('#bbts_link_id').parent().addClass('d-none');
                 }
             }
             function emptyField(){
@@ -454,20 +432,15 @@ legend {
                 let used_capacity = new_capacity - existing_capacity;
                 let terrif_per_month = $('#terrif_per_month').val() ?? 0;
                 let vat_percent = $('#vat_percent').val() ?? 0;
-                
-                
                 if(Math.sign(used_capacity) == -1 && used_capacity != 0){
                     used_capacity = -1 * used_capacity;
                 }
-                
                 amount = used_capacity * terrif_per_month;
-                console.log(amount);
                 if(amount != 0 && vat_percent){
                     var vat = Number(amount) * Number(vat_percent) / 100;
                 }else{
                     var vat = 0;
                 }
-                console.log(vat);
                 $('#vat').val(vat).attr('value',vat);
                 let total = Number(amount) + Number(vat);
                 $('#amount').val(amount).attr('value',amount);
