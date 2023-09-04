227e7dcc4da62eb9f9118bcae2aae6e62550b77f
SHIFUL MAHMUD <shifulmahmudkhan@gmail.com>
2023-09-04 17:50:47 +0600

    Broken Days Bill crud frontend design done

--- Modules/Billing/Entities/BrokenDaysBill.php
+++ Modules/Billing/Entities/BrokenDaysBill.php
@@ -3,8 +3,14 @@
 namespace Modules\Billing\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Sales\Entities\SaleProductDetail;
 
 class BrokenDaysBill extends Model
 {
     protected $guarded = [];
+
+    public function saleProductDetails()
+    {
+        return $this->hasMany(SaleProductDetail::class, 'sale_detail_id', 'id');
+    }
 }
