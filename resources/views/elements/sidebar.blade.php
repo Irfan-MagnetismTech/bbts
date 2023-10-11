7d0b4b2a3de3113f9790bdab02a3ac1d15c88836
WebDevJahir <jahirulislam1994ctgbd@gmail.com>
2023-10-11 12:29:25 +0600

    admin permission given

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -29,72 +29,74 @@
                     </ul>
                 </li>
             @endcan
-            <li
-                class="pcoded-hasmenu {{ request()->routeIs(['branches.*', 'apsections.*', 'teams.*', 'departments.*', 'designations.*', 'employees.*', 'sellCollectionHeads.*', 'services.*']) ? 'active pcoded-trigger' : null }}">
-                <a href="javascript:void(0)">
-                    <span class="pcoded-micon"><i class="ti-settings"></i><b>P</b></span>
-                    <span class="pcoded-mtext">Configurations</span>
-                    <span class="pcoded-mcaret"></span>
-                </a>
-                <ul class="pcoded-submenu">
-                    <li class="{{ request()->routeIs('dataencoding.departments.*') ? 'active' : null }}">
-                        <a href="{{ route('dataencoding.departments.index') }}">
-                            <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
-                            <span class="pcoded-mtext"> Departments </span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
-                    <li class="{{ request()->routeIs('dataencoding.designations.*') ? 'active' : null }}">
-                        <a href="{{ route('dataencoding.designations.index') }}">
-                            <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
-                            <span class="pcoded-mtext"> Designations </span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
-                    <li class="{{ request()->routeIs('dataencoding.employees.*') ? 'active' : null }}">
-                        <a href="{{ route('dataencoding.employees.index') }}">
-                            <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
-                            <span class="pcoded-mtext"> Employee </span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
-                    <li class="{{ request()->routeIs('brands.*') ? 'active' : null }}">
-                        <a href="{{ route('brands.index') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Brands</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('branchs.*') ? 'active' : null }}">
-                        <a href="{{ route('branchs.index') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Branches</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('pops.*') ? 'active' : null }}">
-                        <a href="{{ route('pops.create') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">POP</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('particulars.*') ? 'active' : null }}">
-                        <a href="{{ route('particulars.create') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Particulars</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('connectivity.*') ? 'active' : null }}">
-                        <a href="{{ route('connectivity.create') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Add Link</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('connectivity.*') ? 'active' : null }}">
-                        <a href="{{ route('connectivity.index') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Link List</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                    <li class="{{ request()->routeIs('services.*') ? 'active' : null }}">
-                        <a href="{{ route('services.create') }}"> <span class="pcoded-micon"><i
-                                    class="ti-angle-right"></i></span><span class="pcoded-mtext">Services</span><span
-                                class="pcoded-mcaret"></span></a>
-                    </li>
-                </ul>
-            </li>
+            @can('admin-configuration')
+                <li
+                    class="pcoded-hasmenu {{ request()->routeIs(['branches.*', 'apsections.*', 'teams.*', 'departments.*', 'designations.*', 'employees.*', 'sellCollectionHeads.*', 'services.*']) ? 'active pcoded-trigger' : null }}">
+                    <a href="javascript:void(0)">
+                        <span class="pcoded-micon"><i class="ti-settings"></i><b>P</b></span>
+                        <span class="pcoded-mtext">Configurations</span>
+                        <span class="pcoded-mcaret"></span>
+                    </a>
+                    <ul class="pcoded-submenu">
+                        <li class="{{ request()->routeIs('dataencoding.departments.*') ? 'active' : null }}">
+                            <a href="{{ route('dataencoding.departments.index') }}">
+                                <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
+                                <span class="pcoded-mtext"> Departments </span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
+                        <li class="{{ request()->routeIs('dataencoding.designations.*') ? 'active' : null }}">
+                            <a href="{{ route('dataencoding.designations.index') }}">
+                                <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
+                                <span class="pcoded-mtext"> Designations </span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
+                        <li class="{{ request()->routeIs('dataencoding.employees.*') ? 'active' : null }}">
+                            <a href="{{ route('dataencoding.employees.index') }}">
+                                <span class="pcoded-micon"><i class="icon-pie-chart"></i></span>
+                                <span class="pcoded-mtext"> Employee </span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
+                        <li class="{{ request()->routeIs('brands.*') ? 'active' : null }}">
+                            <a href="{{ route('brands.index') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Brands</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('branchs.*') ? 'active' : null }}">
+                            <a href="{{ route('branchs.index') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Branches</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('pops.*') ? 'active' : null }}">
+                            <a href="{{ route('pops.create') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">POP</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('particulars.*') ? 'active' : null }}">
+                            <a href="{{ route('particulars.create') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Particulars</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('connectivity.*') ? 'active' : null }}">
+                            <a href="{{ route('connectivity.create') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Add Link</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('connectivity.*') ? 'active' : null }}">
+                            <a href="{{ route('connectivity.index') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Link List</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                        <li class="{{ request()->routeIs('services.*') ? 'active' : null }}">
+                            <a href="{{ route('services.create') }}"> <span class="pcoded-micon"><i
+                                        class="ti-angle-right"></i></span><span class="pcoded-mtext">Services</span><span
+                                    class="pcoded-mcaret"></span></a>
+                        </li>
+                    </ul>
+                </li>
+            @endcan
         </ul>
         @can('sales-module')
             @include('sales::layouts.sidebar')
