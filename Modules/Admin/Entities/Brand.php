2d0818933b2db049e1a55e53c828dfa40fb48670
thrivuban51@yahoo.com <thrivuban51@yahoo.com>
2023-10-14 20:29:42 +0600

    activity-log

--- Modules/Admin/Entities/Brand.php
+++ Modules/Admin/Entities/Brand.php
@@ -2,9 +2,12 @@
 
 namespace Modules\Admin\Entities;
 
+use Binafy\LaravelUserMonitoring\Traits\Actionable;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
+    use Actionable;
+
     protected $guarded = [];
 }
